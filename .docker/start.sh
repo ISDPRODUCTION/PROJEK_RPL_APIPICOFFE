28a4c5e747e17f95f502f6173c14e0681d6f795a
Shahrul Alfa Rizky <168643162+ISDPRODUCTION@users.noreply.github.com>
2026-04-10 04:55:36 +0700

    update

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-echo "PORT is: $PORT"
 echo "Starting PHP-FPM..."
 php-fpm &
 sleep 3
-echo "Starting Nginx on port $PORT..."
-envsubst '$PORT' < /etc/nginx/sites-available/default > /etc/nginx/sites-enabled/default
+echo "Starting Nginx..."
 nginx -g "daemon off;" 2>&1
