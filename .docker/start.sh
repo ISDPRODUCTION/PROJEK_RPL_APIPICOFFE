88e0fd7fe65f964dc3fafab8992a620bafd05569
Shahrul Alfa Rizky <168643162+ISDPRODUCTION@users.noreply.github.com>
2026-04-10 04:21:00 +0700

    update

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# Start PHP-FPM in background
-php-fpm -D
+# Start PHP-FPM in foreground mode in background
+php-fpm &
 
-# Wait for PHP-FPM
-sleep 2
-
-# Test nginx config
-nginx -t
+# Wait longer for PHP-FPM to fully start
+sleep 5
 
 # Start Nginx in foreground
 exec nginx -g 'daemon off;'
