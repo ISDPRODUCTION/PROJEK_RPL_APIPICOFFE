82f5d43f489ea0baf1592403c00e337e29a9bf14
Shahrul Alfa Rizky <168643162+ISDPRODUCTION@users.noreply.github.com>
2026-04-10 04:38:27 +0700

    update

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 echo "Starting PHP-FPM..."
 php-fpm &
-FPM_PID=$!
-echo "PHP-FPM started with PID $FPM_PID"
 sleep 3
 echo "Starting Nginx..."
-nginx -g "daemon off;"
-echo "Nginx exit code: $?"
+nginx -g "daemon off;" 2>&1
+echo "Done: $?"
