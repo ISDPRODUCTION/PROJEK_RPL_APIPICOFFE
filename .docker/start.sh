1871a8170153fffa00cd6a0d9609d1c703f2c459
Shahrul Alfa Rizky <168643162+ISDPRODUCTION@users.noreply.github.com>
2026-04-10 06:05:35 +0700

    fix: use file session and fix permissions

--- .docker/start.sh
+++ .docker/start.sh
@@ -3,6 +3,6 @@ php-fpm &
 sleep 3
 php artisan config:clear
 php artisan cache:clear
-php artisan session:table 2>/dev/null || true
+chmod -R 777 /var/www/html/storage
 php artisan migrate --force
 nginx -g "daemon off;" 2>&1
