1013e9893ab14f688b7b9387b6fae534af84eb98
Shahrul Alfa Rizky <168643162+ISDPRODUCTION@users.noreply.github.com>
2026-04-10 13:27:10 +0700

    update

--- .docker/start.sh
+++ .docker/start.sh
@@ -18,15 +18,23 @@ SESSION_DRIVER="${SESSION_DRIVER}"
 SESSION_LIFETIME="${SESSION_LIFETIME}"
 SESSION_SECURE_COOKIE=false
 SESSION_SAME_SITE=none
+AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
+AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
+AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}"
+AWS_BUCKET="${AWS_BUCKET}"
+AWS_ENDPOINT="${AWS_ENDPOINT}"
+AWS_URL="${AWS_URL}"
+AWS_USE_PATH_STYLE_ENDPOINT="${AWS_USE_PATH_STYLE_ENDPOINT}"
+FILESYSTEM_DISK="${FILESYSTEM_DISK}"
 EOF
 
 mkdir -p /var/www/html/storage/framework/sessions
 mkdir -p /var/www/html/storage/framework/cache
 mkdir -p /var/www/html/storage/framework/views
+mkdir -p /var/www/html/storage/logs
 chmod -R 777 /var/www/html/storage
 chown -R www-data:www-data /var/www/html/storage
 php artisan config:clear
 php artisan cache:clear
 php artisan migrate --force
-php artisan db:seed --force
 nginx -g "daemon off;" 2>&1
